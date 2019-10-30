478338e1a99017cf91f7104e3aad8f307a932a2a
Yibo Zhuang <yibzhuang@gmail.com>
2019-10-29 23:30:09 -0700

    simple songs app using Redux

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+  return [
+    { title: 'No scrubs', duration: '4:05' },
+    { title: 'Tethe', duration: '2:30' },
+    { title: 'I want it that way', duratoon: '4:32' },
+    { title: 'All star', duration: '3:32' }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
