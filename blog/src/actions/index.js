63a6f19596d40d224f9ecefd24f7802c6eb97555
Yibo Zhuang <yibzhuang@gmail.com>
2019-10-29 23:39:32 -0700

    Redux for blog posts with jsonplaceholder

---
+++ blog/src/actions/index.js
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+import jsonPlacerholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+};
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlacerholder.get('/posts');
+
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
+};
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlacerholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
